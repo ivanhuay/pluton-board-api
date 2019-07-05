87d8d3849a5d2303a83f647180dbc9c2beaa620d
Ivan Huayraña <ivanhuay@gmail.com>
2019-07-05 15:22:43 -0300

    #7 basic login with github working fine

--- config/public.js
+++ config/public.js
@@ -1,5 +1,5 @@
 'use strict';
-const publicPaths = ['api/auth/login', 'api/auth/signup','api/tickets', 'api/boards'];
+const publicPaths = ['api/auth/login', 'api/auth/signup', 'api/tickets', 'api/boards', 'api/auth/oauth/*'];
 
 const prefix = '^\/';
 const pathRegexStr = prefix + publicPaths.map((path) => {
